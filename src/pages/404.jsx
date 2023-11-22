f32f585d2cc7fc1c5cb9228ac4c0fce64b8a2fd5
siddharth vishen <42781456+siddharth7866@users.noreply.github.com>
2023-11-22 06:36:34 +0530

    first commit

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -3,7 +3,7 @@ import Layout from "@lekoarts/gatsby-theme-minimal-blog/src/components/layout"
 
 const NotFound = () => (
   <Layout>
-    <h1>404 - Page Not Found</h1>
+    <h1>404 - Page Not  Available Found</h1>
     <p>Unfortunately we couldn't find what you were looking for :(</p>
   </Layout>
 )
